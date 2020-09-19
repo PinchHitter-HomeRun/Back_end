84d2529b4dd3364364cc6b09ff0a5a515bd59bc0
ojang <sda362002@naver.com>
2020-09-19 18:01:49 +0900

    Upload init

---
+++ src/main/java/com/toyproj/pinchhitterhomerun/PinchhitterHomerunApplication.java
@@ -0,0 +1,13 @@
+package com.toyproj.pinchhitterhomerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinchhitterHomerunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinchhitterHomerunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyproj/pinchhitterhomerun/PinchhitterHomerunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproj.pinchhitterhomerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinchhitterHomerunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
