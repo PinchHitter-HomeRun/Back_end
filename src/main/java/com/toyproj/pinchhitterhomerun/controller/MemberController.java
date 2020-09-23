37c443bef67c2024f556b7aa7de8729ab898a87c
ojang <sda362002@naver.com>
2020-09-23 16:50:44 +0900

    test with postman

--- src/main/java/com/toyproj/pinchhitterhomerun/controller/MemberController.java
+++ src/main/java/com/toyproj/pinchhitterhomerun/controller/MemberController.java
@@ -2,16 +2,20 @@ package com.toyproj.pinchhitterhomerun.controller;
 
 import com.toyproj.pinchhitterhomerun.model.Member;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class MemberController {
 
-    @GetMapping("/user/{userId}")
+    @GetMapping("/{userId}")
     public String test(@PathVariable String userId) {
         return userId;
+    }
+
+    @PostMapping("/")
+    public String test1(@RequestParam(value="msg") String msg) {
+        return msg;
     }
 
 
--- src/main/java/com/toyproj/pinchhitterhomerun/repository/MemberRepository.java
+++ src/main/java/com/toyproj/pinchhitterhomerun/repository/MemberRepository.java
@@ -2,10 +2,12 @@ package com.toyproj.pinchhitterhomerun.repository;
 
 
 import com.toyproj.pinchhitterhomerun.model.Member;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@Repository
 public class MemberRepository implements IMemberRepository {
 
     @PersistenceContext
