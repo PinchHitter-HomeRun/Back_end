6fb1a9aaa6b77f256799f70c066586358ef8a2e7
ojang <sda362002@naver.com>
2020-09-23 16:42:10 +0900

    domain parsing test

--- src/main/java/com/toyproj/pinchhitterhomerun/controller/MemberController.java
+++ src/main/java/com/toyproj/pinchhitterhomerun/controller/MemberController.java
@@ -3,14 +3,15 @@ package com.toyproj.pinchhitterhomerun.controller;
 import com.toyproj.pinchhitterhomerun.model.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
 
-    @GetMapping("/user/test")
-    public String test() {
-        return "hi2please2222";
+    @GetMapping("/user/{userId}")
+    public String test(@PathVariable String userId) {
+        return userId;
     }
 
 
--- src/main/java/com/toyproj/pinchhitterhomerun/repository/MemberRepository.java
+++ src/main/java/com/toyproj/pinchhitterhomerun/repository/MemberRepository.java
@@ -1,5 +1,19 @@
 package com.toyproj.pinchhitterhomerun.repository;
 
 
+import com.toyproj.pinchhitterhomerun.model.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 public class MemberRepository implements IMemberRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
 }
